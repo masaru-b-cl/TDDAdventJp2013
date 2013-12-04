31c0e364322c9224a8e7a96375c0ef9a03ed9ef3
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-12-05 00:15:50 +0900

    テストケース名による分類サンプルコードを追加

---
+++ TddAdventJp2013.Test/テストケース名.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddAdventJp2013.Test
+{
+  [TestClass]
+  public class テストケース名
+  {
+    [TestMethod]
+    public void Aがa1の場合・Bがb1ならばc1となる()
+    {
+    }
+
+    [TestMethod]
+    public void Aがa1の場合・Bがb2ならばc2となる()
+    {
+    }
+    [TestMethod]
+    public void Aがa2の場合・Bがb1ならばc3となる()
+    {
+    }
+  }
+}
