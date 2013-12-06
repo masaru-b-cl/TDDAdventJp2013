817deae80eb7bf41a452864b17d9c669c55464d9
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-12-07 00:53:16 +0900

    カテゴリーによる分類サンプルを作成

---
+++ TddAdventJp2013.Test/カテゴリー.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddAdventJp2013.Test
+{
+  [TestClass]
+  public class カテゴリー
+  {
+    [TestCategory("Aがa1の場合")]
+    [TestMethod]
+    public void Bがb1ならばc1となる()
+    {
+    }
+
+    [TestCategory("Aがa1の場合")]
+    [TestMethod]
+    public void Bがb2ならばc2となる()
+    {
+    }
+    
+    [TestCategory("Aがa2の場合")]
+    [TestMethod]
+    public void Bがb1ならばc3となる()
+    {
+    }
+  }
+}
