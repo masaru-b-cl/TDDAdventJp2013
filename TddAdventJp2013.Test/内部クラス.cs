98eb92c6a1506b1efda07b293d3942b960d730cb
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-12-07 00:58:53 +0900

    内部クラス名による分類サンプル作成

---
+++ TddAdventJp2013.Test/内部クラス.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddAdventJp2013.Test
+{
+  public class 内部クラス
+  {
+    [TestClass]
+    public class Aがa1の場合
+    {
+      [TestMethod]
+      public void Bがb1ならばc1となる()
+      {
+      }
+
+      [TestMethod]
+      public void Bがb2ならばc2となる()
+      {
+      }
+    }
+
+    [TestClass]
+    public class Aがa2の場合
+    {
+      [TestMethod]
+      public void Bがb1ならばc3となる()
+      {
+      }
+    }
+  }
+}
